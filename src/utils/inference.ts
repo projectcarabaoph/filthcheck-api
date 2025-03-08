45232a297b4e41c047b66a351697ffc8e80e8434
josephencila <joseph.mesia.encila@gmail.com>
2025-03-08 22:32:51 +0800

    feat: utils inference

---
+++ src/utils/inference.ts
@@ -0,0 +1,20 @@
+import path from "path";
+import * as ort from 'onnxruntime-node';
+
+const inference = async (inputTensor: ort.Tensor) => {
+
+    const modelPath = path.join(process.cwd(), "models", "model_q4.onnx");
+
+    const session = await ort.InferenceSession.create(modelPath);
+
+    // Run inference
+    const results = await session.run({ [session.inputNames[0]]: inputTensor });
+
+    // Extract logits
+    const logits = results.logits.data;
+
+    return logits
+
+}
+
+export default inference
