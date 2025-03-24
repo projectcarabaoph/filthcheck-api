7adb7cc9f27e1e4ff7d6090d5afa7dc80d49e962
josephencila <joseph.mesia.encila@gmail.com>
2025-03-24 21:28:35 +0800

    refactor: api middleware, added types to verify api token return value

--- src/middlewares/api-middleware.ts
+++ src/middlewares/api-middleware.ts
@@ -3,6 +3,7 @@ import { Request, Response, NextFunction } from "express";
 import type { TApiKeys } from "@/types";
 import { verifyApiToken } from "@/utils/misc/verify-api-token";
 import { browserClient } from "@/configs/supabase/browser-client";
+import { JwtPayload } from "jsonwebtoken";
 
 const apiMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
@@ -10,8 +11,7 @@ const apiMiddleware = async (req: Request, res: Response, next: NextFunction): P
 
         if (!apiKey) throw new Error('Forbidden')
 
-        const isValidKey = verifyApiToken(apiKey);
-        console.log(isValidKey)
+        const isValidKey = verifyApiToken(apiKey) as JwtPayload;
 
         if (!isValidKey) throw new Error('Forbidden')
 
@@ -20,7 +20,7 @@ const apiMiddleware = async (req: Request, res: Response, next: NextFunction): P
         const { data, error } = await supabase
             .from('api_keys')
             .select('*')
-            .eq('profile_id', apiKey)
+            .eq('profile_id', isValidKey?.id)
 
             .single<TApiKeys>();
 
--- src/routes/detect-routes.ts
+++ src/routes/detect-routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { detectImageController } from '../controllers/detect-controllers';
+import apiMiddleware from '@/middlewares/api-middleware';
 
 const router = express.Router();
 
-router.post('/image', detectImageController);
+router.post('/image', apiMiddleware, detectImageController);
 
 export default router
