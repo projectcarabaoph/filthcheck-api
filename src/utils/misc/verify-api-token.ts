b8c0369cc206bfc7485aefa2537ee91b348b9485
josephencila <joseph.mesia.encila@gmail.com>
2025-03-24 21:19:52 +0800

    feat: utils, verify api token

---
+++ src/utils/misc/verify-api-token.ts
@@ -0,0 +1,26 @@
+import jwt, { JwtPayload, JsonWebTokenError } from 'jsonwebtoken';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const { JWT_SECRET } = process.env
+
+export const verifyApiToken = (token: string): JwtPayload | string | null => {
+    if (!JWT_SECRET) {
+        console.error('JWT_SECRET is not defined');
+        return null;
+    }
+
+    try {
+        return jwt.verify(token, JWT_SECRET as string);
+    } catch (error) {
+        if (error instanceof JsonWebTokenError) {
+            throw new Error(`Invalid JWT: ${error.message}`);
+        }
+
+        if (error instanceof Error) {
+            throw new Error(`JWT Verification Error: ${error.message}`);
+        }
+    }
+    return null;
+}
