b7723b9df2bfbe975e0e5b5c99791b674312fcea
josephencila <joseph.mesia.encila@gmail.com>
2025-03-26 22:52:41 +0800

    refactor: detect route, implement cors middleware

--- src/routes/detect-routes.ts
+++ src/routes/detect-routes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { detectImageController } from '../controllers/detect-controllers';
 import apiMiddleware from '@/middlewares/api-middleware';
+import { corsMiddleware } from '@/middlewares/cors-middleware';
 
 const router = express.Router();
 
-router.post('/image', apiMiddleware, detectImageController);
+router.post('/image', apiMiddleware, corsMiddleware, detectImageController);
 
 export default router
