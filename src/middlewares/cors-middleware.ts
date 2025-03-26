45175fec1da4dae1920324ebe85972ab4139c8f3
josephencila <joseph.mesia.encila@gmail.com>
2025-03-26 22:42:38 +0800

    refactor: cors middleware, implement try catch

--- src/middlewares/cors-middleware.ts
+++ src/middlewares/cors-middleware.ts
@@ -1,17 +1,21 @@
 import { Request, Response, NextFunction } from "express";
 
-export const corsMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    const origin = req.headers.origin;
+export const corsMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const origin = req.headers.origin;
 
-    if (!origin) return next();
+        if (!origin) return next();
 
-    const allowedDomains = req?.apiKey?.domains ? req?.apiKey?.domains.split(',') : []
-    if (!allowedDomains.includes(origin)) {
-        return res.status(403).json({ error: 'Forbidden.' });
+        const allowedDomains = req?.apiKey?.domains ? req?.apiKey?.domains.split(',') : []
+        if (!allowedDomains.includes(origin)) {
+            throw new Error('Forbidden')
+        }
+
+        res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Credentials', 'true');
+
+        next();
+    } catch (error) {
+        if (error instanceof Error) res.status(400).json({ message: error.message })
     }
-
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-
-    next();
 }
